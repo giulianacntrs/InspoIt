1d39d7d3590a8efd77e72551ae75a3547c765a9e
giulianacntrs <112666134+giulianacntrs@users.noreply.github.com>
2024-09-05 17:09:14 -0300

    Add files via upload

---
+++ homepage/organizaciones/Organizaciones.jsx
@@ -0,0 +1,16 @@
+import style from "./Organizaciones.module.css";
+
+import orgImagen from "@/images/homePage/orgImagen.png";
+
+export default function Organizaciones(){
+    return (
+ <div className={style.Organizaciones}>
+<div className={style.container}></div>
+ </div>
+
+    );
+}
+/* export default function SobreNosotros() {
+  return ( 
+  );
+} */
