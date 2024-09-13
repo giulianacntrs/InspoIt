ab435cab713854ba51a192c7a6f4d36d1a70884e
giulianacntrs <112666134+giulianacntrs@users.noreply.github.com>
2024-09-13 11:34:05 -0300

    Add files via upload

--- homepage/Homepage.jsx
+++ homepage/Homepage.jsx
@@ -1,10 +1,19 @@
 import SobreNosotros from "./sobreNosotros/SobreNosotros";
 import Tipodevoluntariado from "./tiposdevoluntariados/Tipoddevoluntariados";
 import Voluntariado from "./voluntariado/Voluntariado";
+import Presentacion from "./Presentacion/Presentacion";
+import Tutorial from "./Tutorial/Tutorial";
+import Organizaciones from "./organizaciones/Organizaciones";
 import style from "./Homepage.module.css";
 export default function Homepage() {
   return (
     <div className={style.home}>
+      <Presentacion />
+      <hr className={style.homeHr} />
+      <Tutorial />
+      <hr className={style.homeHr} />
+      <Organizaciones />
+      <hr className={style.homeHr} />
       <Voluntariado />
       <hr className={style.homeHr} /> <br />
       <Tipodevoluntariado />
